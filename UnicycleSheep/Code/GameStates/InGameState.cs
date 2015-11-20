f501caa18b8fc680853b1240997a13b8e394f667
Thanduriel <rojendersie@alice.de>
2015-11-20 19:51:57 +0100

    added physics world

--- UnicycleSheep/Code/GameStates/InGameState.cs
+++ UnicycleSheep/Code/GameStates/InGameState.cs
@@ -1,11 +1,24 @@
 ﻿using SFML.Graphics;
-
+using Box2DX.Common;
+using Box2DX.Dynamics;
+using Box2DX.Collision;
 
 namespace UnicycleSheep
 {
     class InGameState : IGameState
     {
-        public GameState update() { return GameState.InGame; }
+        World physicsWorld;
+
+        public InGameState()
+        {
+            AABB aabb = new AABB();
+            physicsWorld = new World(aabb, new Vec2(0.0f, 9.81f), false);
+        }
+        public GameState update() 
+        {
+ 
+            return GameState.InGame; 
+        }
         public void draw(RenderWindow win, View view) { }
         public void drawGUI(GUI gui) { }
     }
