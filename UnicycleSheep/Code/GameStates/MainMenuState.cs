a9268018390333c10b70d29212906ebb6567ad3e
Gorthad <Alexander.Moros@web.de>
2015-11-22 21:15:02 +0100

    commitstuff

--- UnicycleSheep/Code/GameStates/MainMenuState.cs
+++ UnicycleSheep/Code/GameStates/MainMenuState.cs
@@ -1,17 +1,29 @@
 ﻿using SFML.Graphics;
 using SFML.Window;
-
+using SFML.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace UnicycleSheep
 {
     class MainMenuState : IGameState
     {
         Sprite backgroundSprite;
+        Sound BackgroundMusic;
+        Sound Spielanfang;
 
         public MainMenuState()
         {
             backgroundSprite = new Sprite(AssetManager.getTexture(AssetManager.TextureName.MainMenuBackground));
-
+            //BackgroundMusic = new Sound(new SoundBuffer("Assets/BackGround1.wav"));
+            //BackgroundMusic.Loop = true;
+            //BackgroundMusic.Volume = 100;
+            //BackgroundMusic.Pitch = 1.0f; // Geschwindigkeit des liedes
+            //BackgroundMusic.Play();
+           // Spielanfang = new Sound(new SoundBuffer("Assets/Mah.m4a"));
         }
 
         public GameState update() 
