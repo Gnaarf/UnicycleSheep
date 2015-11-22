0a9c7697e88465618e3fd7df6f56d557f82d2742
Thanduriel <rojendersie@alice.de>
2015-11-22 14:11:32 +0100

    head size correctly displayed in debugDraw

--- UnicycleSheep/Code/gameplay/PlayerCharacter.cs
+++ UnicycleSheep/Code/gameplay/PlayerCharacter.cs
@@ -267,7 +267,7 @@ namespace UnicycleSheep
             win.Draw(body_Debug);
 
             //the head
-            SFML.Graphics.CircleShape headDeb = new SFML.Graphics.CircleShape(Vector2.Zero.toScreenCoord().X - Vector2.One.toScreenCoord().X);
+            SFML.Graphics.CircleShape headDeb = new SFML.Graphics.CircleShape(Vector2.Zero.toScreenCoord().X - (0.5f * Vector2.One).toScreenCoord().X);
             headDeb.Origin = Vector2.One * headDeb.Radius;
             headDeb.Position = ((Vector2)chest.GetPosition() + (new Vector2(0, 3)).rotate(chest.GetAngle())).toScreenCoord();
             headDeb.Rotation = -chest.GetAngle() * Helper.RadianToDegree;
